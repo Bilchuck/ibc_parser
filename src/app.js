571c8b7589e6bdb6ea30d938ed07b329ece0ef29
Bilchuck <anton.bilchuk@stylelounge.com>
2018-05-28 16:35:39 +0300

    fix: add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+var cors = require('cors')
 const mizhbankController = require('./controllers/mizhbankController')
 const app = express()
 
+app.use(cors())
 const PORT = process.env.PORT || 3000
 
 app.get('/mizhbank', mizhbankController)
